3eaa1796e4e7aba499cdcf6953ae7267cf7caed7
pjhsk113 <pjhsk113@naver.com>
2020-12-15 21:37:20 +0900

    [feat] etc update

--- payload/etc.ts
+++ payload/etc.ts
@@ -5,20 +5,40 @@ const etc: IEtc.Payload = {
 
   list: [
     {
-      title: 'Capstone Design Contest',
-      subTitle: 'Grand Prize',
-      startedAt: '2015-05',
+      title: 'NextStep 클린코드를 위한 TDD, 리팩토링 with Java',
+      subTitle: '클린코드 작성과 TDD 역량 향상을 위한 강의 수강',
+      startedAt: '2020-08',
+      endedAt: '2020-09',
     },
     {
-      title: 'IT Education Service Activities',
-      subTitle: 'Wrold Friends IT Volunteer',
-      startedAt: '2013-07',
-      endedAt: '2013-08',
+      title: 'Dacon 반도체 박막 두께 분석 경진대회',
+      subTitle: 'TensorFlow 기반 머신러닝 모델링 및 알고리즘 설계, 858팀 참가 / 42등',
+      startedAt: '2020-01',
+      endedAt: '2020-02',
     },
     {
-      title: 'Information Olympiad',
-      subTitle: 'Silver Prize',
-      startedAt: '2008-05',
+      title: '이트리즈시스템 우수 개발인력 양성 인턴쉽',
+      subTitle: '개발 미션 수행을 통한 설계 및 개발 프로세스 경험',
+      startedAt: '2019-10',
+      endedAt: '2019-12',
+    },
+    {
+      title: 'Java 기반 빅데이터 양성 프로그램',
+      subTitle: '최우수 학생 선정 / 두바이 GITEX Technology 컨퍼런스 장학생으로 참여',
+      startedAt: '2019-07',
+      endedAt: '2019-10',
+    },
+    {
+      title: 'CENTRONIX',
+      subTitle: '개발 동아리 활동을 통해 다양한 프로젝트 경험',
+      startedAt: '2017-03',
+      endedAt: '2018-08',
+    },
+    {
+      title: '의무 경찰 만기 전역',
+      subTitle: '경찰 버스 운전병으로 근무',
+      startedAt: '2014-10',
+      endedAt: '2016-07',
     },
   ],
 };
