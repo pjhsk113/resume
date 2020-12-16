0a004766250e3c780c67d13d253e023069c831e6
pjhsk113 <pjhsk113@naver.com>
2020-12-16 21:47:51 +0900

    [feat] education 추가

--- payload/education.ts
+++ payload/education.ts
@@ -5,14 +5,20 @@ const education: IEducation.Payload = {
 
   list: [
     {
-      title: 'Foo University',
-      subTitle: 'Bar Major Bachelor',
-      startedAt: '2010-03',
-      // endedAt: '2010-02',
+      title: '국립 한경대학교',
+      subTitle: '전기전자제어 공학과 - 제어트랙 3.3 / 4.5 졸업',
+      startedAt: '2012-03',
+      endedAt: '2018-08',
     },
     {
-      title: 'Foo High School',
-      subTitle: 'Graduated',
+      title: '신구대학교',
+      subTitle: '컴퓨터 멀티미디어 공학과 4.4 / 4.5 졸업',
+      startedAt: '2010-03',
+      endedAt: '2012-02',
+    },
+    {
+      title: '여의도 고등학교',
+      subTitle: '',
       startedAt: '2007-03',
       endedAt: '2010-02',
     },
