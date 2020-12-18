6cfbff5271a177596a91c82840cf18ff058468c0
pjhsk113 <pjhsk113@naver.com>
2020-12-18 21:14:44 +0900

    [feat] introduce 중간 커밋

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -4,11 +4,8 @@ import { lastestUpdatedAt } from '../package.json';
 const introduce: IIntroduce.Payload = {
   disable: false,
 
-  contents: [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer massa magna, feugiat commodo arcu sed, sagittis sollicitudin tortor. Nullam diam ex, interdum nec lacus vel, ultrices pretium sapien. Proin bibendum non nibh eu aliquet. In sit amet arcu sed nisi tincidunt ultricies. Fusce magna tellus, egestas at pretium quis, rutrum et ante. Phasellus efficitur lectus felis, sed lacinia enim rhoncus ut. Sed id mi vitae neque aliquet porttitor. Fusce dignissim ipsum urna, at finibus lectus sodales ut. Nulla ut purus vestibulum, dapibus tellus consequat, porttitor purus. Nunc egestas tortor ut vehicula mollis.',
-    'Integer ut libero eu libero mattis rutrum at at urna. Integer non turpis ante. Suspendisse vestibulum odio id libero facilisis gravida. In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus. Quisque ornare eget ex ut congue. Nam vel finibus sem. Phasellus id neque nibh. Nullam turpis velit, pharetra vitae mi non, bibendum maximus urna.',
-  ],
-  sign: 'Love poem',
+  contents: ['웹 백엔드 개발 기술을 탐구하고', 'aaaaaaaaaaaa'],
+  sign: 'Jangho Park',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
   latestUpdated: lastestUpdatedAt,
