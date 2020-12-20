1d7c2ff2676e29b781e11286d00bb39895d61e9e
pjhsk113 <pjhsk113@naver.com>
2020-12-20 22:42:24 +0900

    [feat] article 추가

--- payload/article.ts
+++ payload/article.ts
@@ -5,18 +5,44 @@ const article: IArticle.Payload = {
 
   list: [
     {
-      content: "IU's agency announces official online merchandise shop for fans (2020.04.22)",
-      href:
-        'https://www.gmanetwork.com/entertainment/celebritylife/news/62342/ius-agency-announces-official-online-merchandise-shop-for-fans/story',
+      content: '[이펙티브 자바] Item6- 불필요한 객체 생성을 피하라 (2020.12.12)',
+      href: 'https://jjingho.tistory.com/49',
     },
     {
-      content: 'K-pop star IU donates ₩100 million to doctors for coronavirus relief (2020.02.27)',
-      href: 'http://www.koreabiomed.com/news/articleView.html?idxno=7550',
+      content: '[후기] NextStep - TDD, Clean Code with Java (2020.10.20)',
+      href: 'https://jjingho.tistory.com/42',
     },
     {
-      content: 'IU donates 100 million KRW after being inspired by her fans (2019.12.30)',
-      href:
-        'https://www.allkpop.com/article/2019/12/iu-donates-to-100-million-krw-after-being-inspired-by-her-fans',
+      content: '[IntelliJ] Json to DTO 쉽게 만들기- DTO generator (2020.07.25)',
+      href: 'https://jjingho.tistory.com/23',
+    },
+    {
+      content: 'Spring Security (2020.07.12)',
+      href: 'https://jjingho.tistory.com/21',
+    },
+    {
+      content: '[Java] JUnit5을 이용한 테스트 시작하기 (2020.07.03)',
+      href: 'https://jjingho.tistory.com/19',
+    },
+    {
+      content: '[Java] - 직렬화(Serialize) (2020.06.17)',
+      href: 'https://jjingho.tistory.com/16',
+    },
+    {
+      content: '[Spring] @Bean VS @Component 차이점 정리 (2020.05.24)',
+      href: 'https://jjingho.tistory.com/11',
+    },
+    {
+      content: 'Swagger를 이용한 API 문서 자동화 (2020.05.15)',
+      href: 'https://jjingho.tistory.com/8',
+    },
+    {
+      content: '[스프링 핵심기술] - @Autowired (2020.05.12)',
+      href: 'https://jjingho.tistory.com/6',
+    },
+    {
+      content: '[스프링 핵심기술] - Ioc 컨테이너와 Bean (2020.05.09)',
+      href: 'https://jjingho.tistory.com/4',
     },
   ],
 };
